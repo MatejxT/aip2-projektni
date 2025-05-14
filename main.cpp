ca6286ca216a54a7ee49917bfedad4067491a3af
MatejxT <matejxturk@gmail.com>
2025-05-14 09:25:02 +0000

    1.0

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
-#define DG 1
-#include "debug.h"
-
+#include<string.h>
+#include<string>
+#include<algorithm>
+#include<iostream>
+using namespace std;
+struct podacIgre
+{
+    int ploca[7][7];
+    char imeIgrac[50];
+    double vrijeme;
+};
 int main()
 {
-    int a = 5;
-    dv(a);
-
+    while(1)
+    {
+        cout << "1. Pokreni igru" << endl;
+        cout << "2. Pravila igre" << endl;
+        cout << "3. Leaderboard" << endl;
+        cout << "4. Ucitaj igru" << endl;
+        cout << "5. "
+    }
     return 0;
-}kkiefhhfe
+}
